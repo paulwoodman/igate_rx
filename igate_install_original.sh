1ffe915bbbcebe701943b2a06d1345fe63d57ca3
paulwoodman <paulwoodman@users.noreply.github.com>
2026-03-30 08:09:44 -0700

    Update igate_install_original.sh

--- igate_install_original.sh
+++ igate_install_original.sh
@@ -117,7 +117,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/bin/bash -c "rtl_fm -f $FREQ - | direwolf -c $USER_HOME/direwolf.conf -r 24000 -D 1 - > $USER_HOME/direwolf.log"
+ExecStart=/bin/bash -c "rtl_fm -g 49 -f $FREQ - | direwolf -c $USER_HOME/direwolf.conf -r 24000 -D 1 - > $USER_HOME/direwolf.log"
 Restart=always
 User=$USER
 Group=$USER
